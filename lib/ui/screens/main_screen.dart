be18bed508cc596f240a6784b4f7159a959252bb
CW-75 <cw75dev@gmail.com>
2025-12-27 03:59:50 -0400

    Create main screen

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dominote_flutter/ui/widgets/table_button.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -6,11 +7,35 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).colorScheme;
+    final media = MediaQuery.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dominote Flutter', style: TextStyle(color: color.primary)),
+        title: Text(
+          'Dominote Flutter',
+          style: TextStyle(color: color.onPrimary),
+        ),
         centerTitle: true,
-        backgroundColor: color.primaryFixed,
+        backgroundColor: color.primaryFixedDim,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to Dominote Flutter',
+                style: TextStyle(fontSize: 24, color: color.inversePrimary),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: media.orientation == Orientation.portrait
+                    ? media.size.width * 0.8
+                    : media.size.width * 0.5,
+                child: CustomAppBtn(label: 'Create Table', onPressed: () => {}),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
